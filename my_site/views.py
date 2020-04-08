a654f311bb21815ad0496931cbb9af7656724a6b
bej <kkrzysztof2107@gmail.com>
2020-04-08 09:24:19 +0200

    First commit of existing webpage

---
+++ my_site/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'my_site/index.html')
+
+def about(request):
+    return render(request, 'my_site/about.html')
